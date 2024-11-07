801e11a5c50cd43597b0bf9d2e9172ecc97eb065
Ming Chen Lu <mingchenlu@MingdeMacBook-Air.local>
2024-11-07 22:45:02 +0900

    元件拆分元件拆分

---
+++ components/productCard/Price.vue
@@ -0,0 +1,16 @@
+<template>
+  <p class="product-price">{{ price }}</p>
+</template>
+<script setup>
+  defineProps({
+    price: String,
+  })
+</script>
+<style scoped>
+  .product-price {
+    font-size: 16px;
+    color: #007BFF;
+    text-align: center;
+    margin-bottom: 16px;
+  }
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  components: {
+    dirs: [
+      {
+        path: "~/components/global",  
+        global: true,
+      },
+      "~/components",
+    ],
+  },
 })
